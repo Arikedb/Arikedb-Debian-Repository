af65d1b2fb1731f3aefd54606f4a8aba9cac48e7
alejandro <alejandroalfonso1994@gmail.com>
2024-12-21 00:52:04 -0500

    Update repository

--- update.sh
+++ update.sh
@@ -5,25 +5,25 @@ DISTRIBUTION="stable"
 COMPONENT="main"
 
 # Create necessary directory structure
-mkdir -p debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-amd64
-mkdir -p debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-arm64
+mkdir -p dists/${DISTRIBUTION}/${COMPONENT}/binary-amd64
+mkdir -p dists/${DISTRIBUTION}/${COMPONENT}/binary-arm64
 
 # Generate Packages and Packages.gz for amd64
-dpkg-scanpackages --arch amd64 debian/pool/${COMPONENT} > debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-amd64/Packages
-gzip -k -f debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-amd64/Packages
+dpkg-scanpackages --arch amd64 pool/${COMPONENT} > dists/${DISTRIBUTION}/${COMPONENT}/binary-amd64/Packages
+gzip -k -f dists/${DISTRIBUTION}/${COMPONENT}/binary-amd64/Packages
 
 # Generate Packages and Packages.gz for arm64
-dpkg-scanpackages --arch arm64 debian/pool/${COMPONENT} > debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-arm64/Packages
-gzip -k -f debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-arm64/Packages
+dpkg-scanpackages --arch arm64 pool/${COMPONENT} > dists/${DISTRIBUTION}/${COMPONENT}/binary-arm64/Packages
+gzip -k -f dists/${DISTRIBUTION}/${COMPONENT}/binary-arm64/Packages
 
 # Generate Release file
-apt-ftparchive -c apt-ftparchive.conf release debian/dists/${DISTRIBUTION} > debian/dists/${DISTRIBUTION}/Release
+apt-ftparchive -c apt-ftparchive.conf release dists/${DISTRIBUTION} > dists/${DISTRIBUTION}/Release
 
 # Generate Release.gpg and InRelease
-rm -f debian/dists/${DISTRIBUTION}/Release.gpg
-rm -f debian/dists/${DISTRIBUTION}/InRelease
-gpg --default-key "${EMAIL}" -abs -o debian/dists/${DISTRIBUTION}/Release.gpg debian/dists/${DISTRIBUTION}/Release
-gpg --default-key "${EMAIL}" --clearsign -o debian/dists/${DISTRIBUTION}/InRelease debian/dists/${DISTRIBUTION}/Release
+rm -f dists/${DISTRIBUTION}/Release.gpg
+rm -f dists/${DISTRIBUTION}/InRelease
+gpg --default-key "${EMAIL}" -abs -o dists/${DISTRIBUTION}/Release.gpg dists/${DISTRIBUTION}/Release
+gpg --default-key "${EMAIL}" --clearsign -o dists/${DISTRIBUTION}/InRelease dists/${DISTRIBUTION}/Release
 
 # Commit and push changes to GitHub
 git add -A
