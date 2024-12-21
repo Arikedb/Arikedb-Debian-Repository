11f9f9a866df913176ce3b3bb72a5239e5601d1a
alejandro <alejandroalfonso1994@gmail.com>
2024-12-21 00:05:48 -0500

    Update repository

--- update.sh
+++ update.sh
@@ -1,17 +1,29 @@
-!#/bin/bash
+#!/bin/bash
 
 EMAIL="spt.arikedb@gmail"
+DISTRIBUTION="stable"
+COMPONENT="main"
 
-# Packages & Packages.gz
-dpkg-scanpackages --multiversion . > debian/Packages
-gzip -k -f debian/Packages
+# Create necessary directory structure
+mkdir -p debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-amd64
+mkdir -p debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-arm64
 
-# Release, Release.gpg & InRelease
-apt-ftparchive release . > Release
-gpg --default-key "${EMAIL}" -abs -o - Release > debian/Release.gpg
-gpg --default-key "${EMAIL}" --clearsign -o - Release > debian/InRelease
+# Generate Packages and Packages.gz for amd64
+dpkg-scanpackages --arch amd64 debian/pool/${COMPONENT} > debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-amd64/Packages
+gzip -k -f debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-amd64/Packages
 
-# Commit & push
+# Generate Packages and Packages.gz for arm64
+dpkg-scanpackages --arch arm64 debian/pool/${COMPONENT} > debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-arm64/Packages
+gzip -k -f debian/dists/${DISTRIBUTION}/${COMPONENT}/binary-arm64/Packages
+
+# Generate Release file
+apt-ftparchive release debian/dists/${DISTRIBUTION} > debian/dists/${DISTRIBUTION}/Release
+
+# Generate Release.gpg and InRelease
+gpg --default-key "${EMAIL}" -abs -o debian/dists/${DISTRIBUTION}/Release.gpg debian/dists/${DISTRIBUTION}/Release
+gpg --default-key "${EMAIL}" --clearsign -o debian/dists/${DISTRIBUTION}/InRelease debian/dists/${DISTRIBUTION}/Release
+
+# Commit and push changes to GitHub
 git add -A
-git commit -m update
+git commit -m "Update repository"
 git push
